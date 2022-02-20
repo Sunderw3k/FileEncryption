4e7e2fbc1da5f39f74e4f4d87bc3f9de4ff7b4d8
Sunderw3k <sergiejek28@gmail.com>
2022-02-20 20:16:44 +0100

    fix: Fixed wrong port number

--- decrypt.py
+++ decrypt.py
@@ -11,19 +11,18 @@ def main(path, password, ip):
     with open(path, "rb") as file:
         content = file.read()
 
-    r = requests.post("http://{ip}:8000/check", json = {
+    r = requests.post(f"http://{ip}:8000/check", json = {
         "name": str(path),
         "content": str(hashlib.sha256(content).hexdigest()),
         "hash": str(hashlib.sha256(password.encode("UTF-8")).hexdigest())
     })
 
-    if r.text == "\"404\"":
+    if r.text == "404":
         print("Not found!")
         return
-    if r.text == "\"403\"":
+    if r.text == "403":
         print("Password Incorrect!")
-        return
-    
+        return    
     key = r.text[1:-1]
     f = Fernet(key.encode())
     content = f.decrypt(content)
